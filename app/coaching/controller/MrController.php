3d1d152a7889ed1266f8cc0c3f7adb1fec50c9cd
naiu <naiu@nnit.com>
2021-05-07 14:10:51 +0800

    实验

--- app/coaching/controller/MrController.php
+++ app/coaching/controller/MrController.php
@@ -26,22 +26,22 @@ class MrController extends BaseController
    */
   public function index()
   {
-    $visits = $this->visit_model->get_by_initial($this->_initial);
-    $listarr = array();
-    $listarr = $this->visit_model->get_list($this->_initial);
-    //医院名字用 、 隔开 大于15个字用 ... 代替
-    foreach($listarr as &$l){
-      $l['hospitals'] = str_replace('|', '、', $l['hospitals']);
-      if(mb_strlen($l['hospitals'], 'UTF8') > 15 ) {
-        $l['hospitals'] = mb_substr($l['hospitals'],0,15) . '...';
-      }
-      //计算几个医生
-      $l['doctornum'] = count(explode('|', $l['doctors']));
-    }
-    $this->_vdata['list'] = $listarr;
-    //--------ZWGU 1116 ---------------------
-    $this->_vdata['draft'] = $this->cache->file->get($this->_initial);
-    $this->load->view('mr_index', $this->_vdata);
+    // $visits = $this->visit_model->get_by_initial($this->_initial);
+    // $listarr = array();
+    // $listarr = $this->visit_model->get_list($this->_initial);
+    // //医院名字用 、 隔开 大于15个字用 ... 代替
+    // foreach($listarr as &$l){
+    //   $l['hospitals'] = str_replace('|', '、', $l['hospitals']);
+    //   if(mb_strlen($l['hospitals'], 'UTF8') > 15 ) {
+    //     $l['hospitals'] = mb_substr($l['hospitals'],0,15) . '...';
+    //   }
+    //   //计算几个医生
+    //   $l['doctornum'] = count(explode('|', $l['doctors']));
+    // }
+    // $this->_vdata['list'] = $listarr;
+    // //--------ZWGU 1116 ---------------------
+    // $this->_vdata['draft'] = $this->cache->file->get($this->_initial);
+    // $this->load->view('mr_index', $this->_vdata);
   }
 
 }
